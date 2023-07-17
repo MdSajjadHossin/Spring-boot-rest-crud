d2c27ce792f072e2bb6ca4c196b6b69620371e3a
Md Sajjad Hossin <86626194+MdSajjadHossin@users.noreply.github.com>
2023-07-17 17:26:01 +0600

    excemtion handeling error message added

--- demo/src/main/java/com/shossin/demo/rest/StudentRestController.java
+++ demo/src/main/java/com/shossin/demo/rest/StudentRestController.java
@@ -2,6 +2,8 @@ package com.shossin.demo.rest;
 
 import com.shossin.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +44,26 @@ public class StudentRestController {
 
         // just index into the list.... keep it simple for now
 
+        //check the studentId again list size
+
+        if((studentId >= theStudents.size()) || (studentId < 0)){
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
         return theStudents.get(studentId);
+    }
+
+    // Add an exception handler using @exceptionHandler
+
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.getTimeStamp(System.currentTimeMillis());
+        // return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
